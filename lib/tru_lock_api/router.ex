fce2b0fbb67aa855e20af6710522d6f9ecfb75b2
Eric <eric.leinad92@gmail.com>
2017-07-17 00:24:02 -0400

    [Bugfix | Authentication] This includes:
    - Refactoring Auth endpoint to handle missed cases and match the stored
    records correctly

--- lib/tru_lock_api/router.ex
+++ lib/tru_lock_api/router.ex
@@ -61,7 +61,7 @@ defmodule TruLockApi.Router do
 
     with validation = Detective.validate(params),
          {:ok, %{email: email, entity_id: entity_id, image: image}} <- validation,
-         factor_one_auth = Store.hget(entity_id, email),
+         {:ok, factor_one_auth} <-  Store.hget(entity_id, email),
          %{collection: _cid, enrollment: eid, image_count: img_count} <- factor_one_auth,
          true <- img_count > 3,
          {:ok, avg_score} <- TruFace.Workers.Detective.match?(image, eid) do
@@ -78,6 +78,11 @@ defmodule TruLockApi.Router do
           send_resp(conn, 401, response)
       end
     else
+      {:err, nil} ->
+        error_response =
+          %{error: "must be enrolled to authenticate"}
+          |> Poison.encode!()
+        send_resp(conn, 403, error_response)
       false ->
         error_response =
           %{error: "must be finish enrollment to authenticate"}
